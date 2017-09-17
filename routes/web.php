ba1c7bb066a7acc0d2f3008e731160938aa55017
maxles <maxlesunow@gmail.com>
2017-09-17 23:47:54 +0300

    Заявки API

--- routes/web.php
+++ routes/web.php
@@ -72,4 +72,6 @@ Route::group(['middleware' => 'role:admin', 'prefix' => 'api'], function () {
     Route::get('/payment/types', 'PaymentTypeController@getPaymentTypes');
 
     Route::get('/payments', 'PaymentController@getPayments');
+
+    Route::get('/reservations', 'ReservationController@getReservations');
 });
