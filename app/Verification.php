c2086408051737e4a60a39395abdc01f31f9c599
maxles <maxlesunow@gmail.com>
2017-09-20 17:25:39 +0300

    fix delete verification

--- app/Verification.php
+++ app/Verification.php
@@ -3,20 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 
 class Verification extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'code', 'wrong_pass', 'date_expire'
+    ];
+
     public function __construct()
     {
         $this->code = $this->generateCode();
         $this->wrong_pass = 0;
         $this->date_expire = Carbon::now()->addMinute(5);
     }
-
-    protected $fillable = [
-        'code', 'wrong_pass', 'date_expire'
-    ];
 
     protected function generateCode(){
         $code ='';
