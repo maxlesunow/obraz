b00fe76751c938e211b6cf5ba4f4fc9f5fa2db14
maxles <maxlesunow@gmail.com>
2017-09-20 15:15:20 +0300

    add sofdelete user, review, paymentType

--- app/PaymentType.php
+++ app/PaymentType.php
@@ -3,11 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use \Askedio\SoftCascade\Traits\SoftCascadeTrait;
 
 class PaymentType extends Model
 {
+    use SoftDeletes;
+    use SoftCascadeTrait;
+
+    protected $softCascade = ['courses'];
+
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'name', 'type_code', 'is_online'
     ];
 
+    public function reservations(){
+
+        return $this->hasMany('App\Reservation');
+    }
 }
