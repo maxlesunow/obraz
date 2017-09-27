aa3ade4f4042686c483214cb72c702c5d3ee0d56
maxles <maxlesunow@gmail.com>
2017-09-27 11:51:35 +0300

    fix mobile version

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>@yield('title') | Образ</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Styles -->
