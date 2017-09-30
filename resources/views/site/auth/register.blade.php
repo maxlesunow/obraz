f2166af5295e3f8e7f527b9c7ef0fc4fb4afe28c
Иван Пригорнев < >
2017-09-30 13:00:59 +0300

    add restore

--- resources/views/site/auth/register.blade.php
+++ resources/views/site/auth/register.blade.php
@@ -22,8 +22,9 @@
             <!-- Responsive-tabs-->
             <div class="responsive-tabs responsive-tabs-classic" data-type="horizontal">
                 <ul class="resp-tabs-list tabs-1 text-center tabs-group-default" data-group="tabs-group-default">
-                    <li>Login</li>
-                    <li>Registration</li>
+                    <li>Вход</li>
+                    <li>Регистрация</li>
+                    <li>Восстановление</li>
                 </ul>
                 <div class="resp-tabs-container text-left tabs-group-default" data-group="tabs-group-default">
                     <div>
@@ -74,6 +75,34 @@
                                     <div class="form-group offset-top-24">
                                         <label class="form-label form-label-outside" for="form-register-confirm-password">Confirm Password:</label>
                                         <input class="form-control bg-white" id="form-register-confirm-password" type="text" name="confirmpass" data-constraints="@Required">
+                                    </div>
+                                    <div class="offset-top-24">
+                                        <button class="btn btn-primary btn-block" type="submit">create an account</button>
+                                    </div>
+                                </form>
+                                <div class="offset-top-30 text-center">
+                                    <p>or enter with</p>
+                                    <ul class="list-inline">
+                                        <li><a class="icon fa fa-facebook icon-xxs icon-circle icon-darkest-filled" href="#"></a></li>
+                                        <li><a class="icon fa fa-twitter icon-xxs icon-circle icon-darkest-filled" href="#"></a></li>
+                                        <li><a class="icon fa fa-google-plus icon-xxs icon-circle icon-darkest-filled" href="#"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="range range-xs-center section-34">
+                            <div class="cell-xs-8 cell-sm-6 cell-md-4">
+                                <!-- RD Mailform-->
+                                <form class="rd-mailform text-left" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
+                                    <div class="form-group">
+                                        <label class="form-label form-label-outside" for="form-register-username">Name:</label>
+                                        <input class="form-control bg-white" id="form-register-username" type="text" name="name" data-constraints="@Required">
+                                    </div>
+                                    <div class="form-group offset-top-24">
+                                        <label class="form-label form-label-outside" for="form-register-password">Password:</label>
+                                        <input class="form-control bg-white" id="form-register-password" type="text" name="pass" data-constraints="@Required">
                                     </div>
                                     <div class="offset-top-24">
                                         <button class="btn btn-primary btn-block" type="submit">create an account</button>
