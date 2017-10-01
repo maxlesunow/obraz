f16558c2851647d6903b62b3cb738df79d36c769
maxles <maxlesunow@gmail.com>
2017-10-01 14:05:22 +0300

    fix style and home page

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,31 +5,72 @@
 @section('keywords', $page->seo_keywords)
 
 @section('content')
-    {{--LOGO FULL SIZE--}}
-    <section class="section-top-50" style="overflow: hidden;">
-        <div class="mock-up-wrapper veil reveal-sm-inline-block">
-            <div><img class="mock-up-desktop img-responsive" src="images/logos/logo-full.png" height="300" alt=""></div>
-        </div>
-    </section>
-
-    {{--TEXT--}}
-    <section class="section-50 section-sm-top-50 section-sm-bottom-50 section-mock-up" id="home-section-welcome">
+    <section class="offset-top-66 offset-md-top-66">
         <div class="shell">
-            <div class="range range-xs-center">
-                <div class="cell-md-10">
-                    <h3>Консалтинговое пространство Образ</h3>
-                    <p>это дружеское
-                        объединение, в истоках которого стоят специалисты, добившиеся
-                        успеха в актуальных направлениях деятельности с нуля, проходя
-                        иерархию развития и набирая опыт только с положительной динамикой.</p>
+            <h2>Консалтинговое пространство &laquo;Образ&raquo;</h2>
+            <p>Это дружеское
+                объединение, в истоках которого стоят специалисты, добившиеся
+                успеха в актуальных направлениях деятельности с нуля, проходя
+                иерархию развития и набирая опыт только с положительной динамикой.</p>
+            <div class="range range-md-middle offset-top-34">
+                <div class="cell-md-4 cell-md-push-1"><img class="center-block img-responsive" src="images/logos/logo-full.png" alt="" height="470"></div>
+                <div class="cell-sm-8 cell-sm-preffix-2 cell-md-4 cell-md-preffix-0 text-md-right">
+                    <div class="unit unit-spacing-sm unit-inverse unit-md unit-md-horizontal">
+                        <div class="unit-body">
+                            <h4 class="text-uppercase text-bold offset-md-top-20">Стоматология</h4>
+                            <p>The information should be not only well-organized and easy to read. thus making the process enjoyable.</p>
+                        </div>
+                        <div class="unit-right"><span class="icon icon-circle icon-lg icon-bordered icon-picton-blue mdi mdi-message-text-outline"></span></div>
+                    </div>
+                    <div class="unit unit-spacing-sm unit-inverse unit-md unit-md-horizontal offset-top-50 offset-md-top-30">
+                        <div class="unit-body">
+                            <h4 class="text-uppercase text-bold offset-md-top-20">Маникюр</h4>
+                            <p>The information should be not only well-organized and easy to read. thus making the process enjoyable.</p>
+                        </div>
+                        <div class="unit-right"><span class="icon icon-circle icon-lg icon-bordered icon-picton-blue mdi mdi-image-filter"></span></div>
+                    </div>
+                    <div class="unit unit-spacing-sm unit-inverse unit-md unit-md-horizontal offset-top-50 offset-md-top-30">
+                        <div class="unit-body">
+                            <h4 class="text-uppercase text-bold offset-md-top-20">Педикюр</h4>
+                            <p>The information should be not only well-organized and easy to read. thus making the process enjoyable.</p>
+                        </div>
+                        <div class="unit-right"><span class="icon icon-circle icon-lg icon-bordered icon-picton-blue mdi mdi-emoticon-happy"></span></div>
+                    </div>
+                </div>
+                <div class="cell-sm-8 cell-sm-preffix-2 cell-md-4 cell-md-preffix-0 text-md-left cell-md-push-1 offset-top-50 offset-md-top-0">
+                    <div class="unit unit-spacing-sm unit-md unit-md-horizontal">
+                        <div class="unit-left"><span class="icon icon-circle icon-lg icon-bordered icon-picton-blue mdi mdi-cellphone-link"></span></div>
+                        <div class="unit-body">
+                            <h4 class="text-uppercase text-bold offset-md-top-20">Курсы</h4>
+                            <p>The information should be not only well-organized and easy to read. thus making the process enjoyable.</p>
+                        </div>
+                    </div>
+                    <div class="unit unit-spacing-sm unit-md unit-md-horizontal offset-top-50 offset-md-top-30">
+                        <div class="unit-left"><span class="icon icon-circle icon-lg icon-bordered icon-picton-blue mdi mdi-television-guide"></span></div>
+                        <div class="unit-body">
+                            <h4 class="text-uppercase text-bold offset-md-top-20">Тренинги</h4>
+                            <p>The information should be not only well-organized and easy to read. thus making the process enjoyable.</p>
+                        </div>
+                    </div>
+                    <div class="unit unit-spacing-sm unit-md unit-md-horizontal offset-top-50 offset-md-top-30">
+                        <div class="unit-left"><span class="icon icon-circle icon-lg icon-bordered icon-picton-blue mdi mdi-play-box-outline"></span></div>
+                        <div class="unit-body">
+                            <h4 class="text-uppercase text-bold offset-md-top-20">Вебинары</h4>
+                            <p>
+                                The information should be not only well-organized and easy to read. thus making the process enjoyable.
+
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
+    </section>
 
     <!--DISCOUNT-->
-    <section class="context-dark">
-        <section class="section-50 section-top-45 bg-mantis section-triangle section-triangle-bottom">
+    <section class="context-dark section-66 section-top-66">
+        <section class="section-66 section-top-66 bg-mantis section-triangle section-triangle-bottom">
             <div class="shell">
                 <div class="range range-sm-center">
                     <h2><span class="big">Скидка 15% на первый заказ курса</span></h2>
@@ -44,8 +85,8 @@
             <svg class="svg-triangle-bottom" xmlns="http://www.w3.org/2000/svg" version="1.1">
                 <defs>
                     <lineargradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-                        <stop offset="0%" style="stop-color:rgb(99,189,98);stop-opacity:1;"></stop>
-                        <stop offset="100%" style="stop-color:rgb(99,189,98);stop-opacity:1;"></stop>
+                        <stop offset="0%" style="stop-color:rgb(71,149,213);stop-opacity:1;"></stop>
+                        <stop offset="100%" style="stop-color:rgb(71,149,213);stop-opacity:1;"></stop>
                     </lineargradient>
                 </defs>
                 <polyline points="0,0 60,0 29,29" fill="url(#grad1)"></polyline>
