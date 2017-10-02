217ff5fb6d0788285b2cf0044eff36063014daeb
maxles <maxlesunow@gmail.com>
2017-10-02 16:40:51 +0300

    api course filter

--- app/Http/Controllers/Site/CourseController.php
+++ app/Http/Controllers/Site/CourseController.php
@@ -44,41 +44,111 @@ class CourseController extends Controller
 
         $query = Course::query();
 
-        $query->with(['speakers' => function($q) {
-            $q-> select('first_name', 'last_name', 'middle_name');
-        }]);
+        $query->with([
 
-        $query->join('course_types', 'courses.course_type_id', '=', 'course_types.id');
-        $query->join('course_groups', 'courses.course_group_id', '=', 'course_groups.id');
+            'course_type' => function($q) {
+                $q-> select('id','name');
+            },
 
+            'course_group' => function($q) {
+                $q-> select('id','name');
+            },
 
-//        $query->select(
-//            'courses.name',
-//            'courses.address',
-//            'courses.cost',
-//            'courses.time_register',
-//            'courses.time_start',
-//            'courses.description',
-//            'courses.image',
-//            'course_types.name as course_type_name',
-//            'course_groups.name as course_group_name',
-//        );
+            'speakers' => function($q) {
+                $q-> select('id','first_name', 'last_name', 'middle_name');
+            }
+        ]);
+
+        $query->select(
+            'id',
+            'courses.course_type_id',
+            'courses.course_group_id',
+            'courses.name',
+            'courses.address',
+            'courses.cost',
+            'courses.time_register',
+            'courses.time_start',
+            'courses.description',
+            'courses.image'
+        );
 
         $query->orderBy('time_start', 'desc');
 
-        //Берем только будущие курсы
-        $query->where('time_start', '>', Carbon::now());
 
-//        //Фильтрация
-//        if ($request->exists('filter')) {
-//            $query->where(function($q) use($request) {
-//                $value = "%{$request->filter}%";
-//                $q->where('name', 'ilike', $value);
-//            });
-//        }
+        //Фильтрация
+        if ($request->exists('filter')) {
+            $filters = explode(',', request()->filter);
+            foreach ($filters as $filter) {
+                if($filter) {
+                    list($filterBy, $filterValues) = explode('|', $filter);
+                    $values = explode(':', $filterValues);
+
+                    $query->where(function ($q) use ($filterBy, $values) {
+                        foreach ($values as $value) {
+                            $q->orWhere($filterBy, $value);
+                        }
+                    });
+                }
+            }
+        }
+
+        //Берем только будущие курсы, либо фильтруем по дате
+        if ($request->exists('date_start') && $request->exists('date_stop')) {
+
+            if(Carbon::now() > Carbon::parse($request->date_start)){
+
+                $query->whereBetween('time_start', [Carbon::now(), Carbon::parse($request->date_stop)]);
+            }
+            else{
+
+                $query->whereBetween('time_start', [Carbon::parse($request->date_start), Carbon::parse($request->date_stop)]);
+            }
+        }
+        elseif ($request->exists('date_start') && !$request->exists('date_stop')){
+
+            if(Carbon::now() > Carbon::parse($request->date_start)){
+
+                $query->where('time_start', '>', Carbon::now());
+            }
+            else{
+
+                $query->where('time_start', '>', Carbon::parse($request->date_start));
+            }
+
+            $query->where('time_start', '>', Carbon::parse($request->date_start));
+        }
+        elseif (!$request->exists('date_start') && $request->exists('date_stop')){
+
+            $query->whereBetween('time_start', [Carbon::now(), Carbon::parse($request->date_stop)]);
+        }
+
         //Пагинация
         $perPage = request()->has('per_page') ? (int) request()->per_page : null;
         $pagination = $query->paginate($perPage);
+
+
+//        Скрываем лишние поля
+        $set_hidden_course = [
+            'id',
+            'reservations_count',
+            'course_group_id',
+            'course_type_id',
+        ];
+        $set_hidden_speaker = ['courses_count', 'pivot', 'id'];
+        $set_hidden_course_group = ['id'];
+        $set_hidden_course_type = ['id'];
+
+        $pagination->each(function($row) use ($set_hidden_course, $set_hidden_speaker, $set_hidden_course_group, $set_hidden_course_type) {
+
+            $row->setHidden($set_hidden_course);
+            $row->speakers->each(function($row) use ($set_hidden_speaker) {
+                $row->setHidden($set_hidden_speaker);
+            });
+            $row->course_group->setHidden($set_hidden_course_group);
+            $row->course_type->setHidden($set_hidden_course_type);
+        });
+
+//        Добавляем пагинацию
         $pagination->appends([
             'sort' => request()->sort,
             'filter' => request()->filter,
