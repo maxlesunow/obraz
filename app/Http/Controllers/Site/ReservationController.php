5983ddc12a342b254179042a209171caabd2adf0
maxles <maxlesunow@gmail.com>
2017-10-05 13:48:39 +0300

    fix sort full_name to reviews

---
+++ app/Http/Controllers/Site/ReservationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReservationController extends Controller
+{
+    public function store(ReviewRequest $request)
+    {
+        $review = new Review($request->all());
+
+        $review->date = Carbon::now();
+        $review->user()->associate(Auth::user());
+        $review->save();
+
+        return response()->json($review, 200);
+    }
+}
