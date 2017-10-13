0725ce64d3862949262454c4101bcb2cf7cc6775
v1ar31 <v1ar31@gmail.com>
2017-10-14 00:24:46 +0300

    add tables

--- resources/views/site/cabinet/reservations.blade.php
+++ resources/views/site/cabinet/reservations.blade.php
@@ -14,5 +14,56 @@
 @endsection
 
 @section('content')
-    <h1>Мои курсы</h1>
+<section class="section-124">
+    <div class="shell">
+        <div class="content">
+            <div class="cell-xs-8">
+                <h3>Мои курсы</h3>
+
+                <table class="table table-custom" data-responsive="true">
+                    <tr>
+                        <th>Название</th>
+                        <th>Время начала</th>
+                        <th>Стоимость</th>
+                        <th>Способ оплаты</th>
+                        <th>Статус оплаты</th>
+                        <th>Билет</th>
+                    </tr>
+                    <tr>
+                        <td>Sergey Gaponov</td>
+                        <td>Gap0n</td>
+                        <td>**********</td>
+                        <td>haponov.serhii@gmail.com</td>
+                        <td>**********</td>
+                        <td>**********</td>
+                    </tr>
+                    <tr>
+                        <td>Eugene Gusarov</td>
+                        <td>Diversant</td>
+                        <td>*******</td>
+                        <td>stmechanus@gmail.com</td>
+                        <td>**********</td>
+                        <td>**********</td>
+                    </tr>
+                    <tr>
+                        <td>Vladislav Gnatovsky</td>
+                        <td>Mason</td>
+                        <td>*********</td>
+                        <td>vgtsky@gmail.com</td>
+                        <td>**********</td>
+                        <td>**********</td>
+                    </tr>
+                    <tr>
+                        <td>Rafael Shayvolodyan</td>
+                        <td>Raffa</td>
+                        <td>********</td>
+                        <td>mnatis.rafael@gmail.com</td>
+                        <td>**********</td>
+                        <td>**********</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
